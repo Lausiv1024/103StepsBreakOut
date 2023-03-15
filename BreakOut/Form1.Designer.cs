c65ffe733a02c4955ed32a90c931b778c3b1435d
lausiv1024 <kouki7x2dant@gmail.com>
2023-03-16 08:36:46 +0900

    プロジェクト ファイルを追加します。

---
+++ BreakOut/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BreakOut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Title = new System.Windows.Forms.Label();
+            this.Bouncer = new System.Windows.Forms.Label();
+            this.Ball1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Title
+            // 
+            resources.ApplyResources(this.Title, "Title");
+            this.Title.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Title.Name = "Title";
+            // 
+            // Bouncer
+            // 
+            this.Bouncer.BackColor = System.Drawing.Color.Yellow;
+            resources.ApplyResources(this.Bouncer, "Bouncer");
+            this.Bouncer.Name = "Bouncer";
+            // 
+            // Ball1
+            // 
+            this.Ball1.Image = global::BreakOut.Properties.Resources.circle;
+            resources.ApplyResources(this.Ball1, "Ball1");
+            this.Ball1.Name = "Ball1";
+            this.Ball1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.Controls.Add(this.Ball1);
+            this.Controls.Add(this.Bouncer);
+            this.Controls.Add(this.Title);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Bouncer;
+        private System.Windows.Forms.PictureBox Ball1;
+    }
+}
+
---
+++ BreakOut/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BreakOut
+{
+    public partial class Form1 : Form
+    {
+        private int bouncerPos = 0;
+        private int ballPosX = 0, ballPosY = 0, ballSpeedX = 0, ballSpeedY = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!timer1.Enabled && e.KeyCode == Keys.Space)//ゲームが開始しているかはタイマーの状態で判断する
+            {
+                Title.Text = string.Empty;
+                ballPosX = Ball1.Left; 
+                ballPosY = Ball1.Top;
+                double a = new Random().NextDouble() * 5.8 + 1;
+                ballSpeedX =(int) (Math.Sin(a) * 10);
+                ballSpeedY=(int) (Math.Sin(a)/ 10);
+
+
+                timer1.Start();
+            }
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+            if (!timer1.Enabled) 
+                return;
+            if (e.KeyCode == Keys.Left && (Math.Abs(bouncerPos) < 3 || bouncerPos == 3))
+            {
+                bouncerPos--;
+            }
+            if (e.KeyCode == Keys.Right && (Math.Abs(bouncerPos) < 3 || bouncerPos == -3))
+            {
+                bouncerPos++;
+            }
+
+            Bouncer.Location = new Point(posHolder[bouncerPos + 3], 360);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        int[] posHolder = new int[] { 0, 130, 230, 330, 430, 530, 660};
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
